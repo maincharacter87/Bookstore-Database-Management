23325541e873d5b3a1ada0e3368d60eb8cb76704
Jane Swingler <jswingler007@gmail>
2023-05-01 11:35:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# bookish-computing-machine
+## Database Management System for a Bookstore Clerk
+This is a basic bookstore management program written in Python and using SQLite as the database. This program allows you to manage the inventory of books in a bookstore by adding, updating, and deleting books. It also provides functionality to search for books based on various criteria.
+
+## Installation
+1. Clone or download the program files to your computer.
+2. Install Python (version 3.6 or higher) from the official website: https://www.python.org/downloads/
+3. Open a terminal or command prompt and navigate to the program's directory.
+4. Install the required Python packages by running the following command:
+    **pip install -r requirements.txt**
+5. Run the program by executing the following command:
+    **python bookstore.py**
+
+## Usage
+1.	Make sure you have completed the installation steps mentioned in the previous section.
+2.	Open a terminal or command prompt and navigate to the program's directory.
+3.	Run the program.
+4.  The program will start and display a menu with options to manage the bookstore inventory:
+
+![menu](https://user-images.githubusercontent.com/130300157/235342375-a093d4e8-3932-44b4-b4e8-6575dc886bcd.png)
+
+5. Follow the on-screen prompts to perform various operations such as adding books, updating book information, deleting books, and searching for books.
+6. Use the appropriate menu options and input the required information when prompted.
+7. The program will interactively guide you through the process and provide feedback on the operations performed.
+
+## Credits
+I would like to acknowledge the following their contributions to this project:
+* GitHub for providing a platform to host and collaborate on this project.
+* Python for the powerful programming language used in this project.
+* SQLite for the lightweight and efficient database system utilized in this project.
+* The numerous open-source projects and communities whose libraries and resources have been utilized throughout the development of this project.
+
+I express my gratitude to all the individuals and organizations mentioned above for their valuable contributions and support.
---
+++ bookstore.py
@@ -0,0 +1,98 @@
+# =================================== CAPSTONE PROJECT: BOOKSTORE ===================================
+
+import sqlite3
+
+# Create a database called ebookstore
+db = sqlite3.connect('ebookstore')
+
+cursor = db.cursor()
+
+# Create a table called books
+cursor.execute('''CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, qty INTEGER)
+''')
+db.commit()
+
+book_data = [(3001, "A Tale of Two Cities", "Charles Dickens", 30), (3002, "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 40), (3003, "The Lion, the Witch and the Wardrobe", "C.S. Lewis", 25), (3004, "The Lord of the Rings", "J.R.R Tolkien", 37), (3005, "Alice in Wonderland", "Lewis Carroll", 12)]
+
+# Insert rows from book_data
+cursor.executemany('''INSERT INTO books VALUES(?,?,?,?)''', book_data)
+db.commit()
+
+# Enter a new book function
+def enter_book():
+      id_ = int(input("Enter the book ID: "))
+      title_ = input("Enter the book ttle: ")
+      author_ = input("Enter the book author: ")
+      qty_ = int(input("Enter the quantity of books: "))
+      cursor.execute('''INSERT INTO books(id, title, author, qty) VALUES(?,?,?,?)''', (id_, title_, author_, qty_))
+      db.commit()
+      print("Book successfully entered.")
+
+# Update a book function
+def update_book():
+    id_ = int(input("Enter the ID of the book you would like to update: "))
+    choice = input('''
+    What would you like to update (select 1, 2, or 3):
+    1.  Title
+    2.  Author
+    3.  Quantity''').strip()
+    if choice == "1":
+        title_ = input("Enter the updated title: ")
+        cursor.execute('''UPDATE books SET title = ? WHERE title = ?''', (title_, id_))
+        db.commit()
+        print("Book title updated.")
+    elif choice == "2":
+        author_ = input("Enter the updated author: ")
+        cursor.execute('''UPDATE books SET author = ? WHERE id = ?''', (author_, id_))
+        db.commit()
+        print("Book author updated.")
+    elif choice == "3":
+        qty_ = int(input("Enter the updated quantity of books in stock: "))
+        cursor.execute('''UPDATE books SET qty = ? WHERE id = ?''', (qty_, id_))
+        db.commit()
+        print("Book quantity updated.")
+    else:
+        print("Oops! Invalid entry.")
+
+# Delete a book function
+def delete_book():
+    id_ = int(input("Enter the ID of the book to delete: "))
+    cursor.execute('''DELETE FROM books WHERE id = ?''', (id_,))
+    db.commit()
+    print("Book deleted.")
+
+# Search a book function by title or author
+def search_book():
+    choice = int(input('''
+    Enter the book ID: ''').strip())
+    cursor.execute('''SELECT id, title, author, qty FROM books WHERE id = ?''', (choice,))
+    book = cursor.fetchone()
+    print(book)
+
+# Display menu options to user
+while True:
+    user_choice = input('''
+Welcome to the ebookstore database management system!
+Enter the number to select the action
+1. Enter book
+2. Update book
+3. Delete book
+4. Search books
+5. Exit
+:
+    ''').strip()
+
+    if user_choice == "1":
+        enter_book()
+    elif user_choice == "2":
+        update_book()
+    elif user_choice == "3":
+        delete_book()
+    elif user_choice == "4":
+        search_book()
+    elif user_choice == "5":
+        db.close()
+        print("Connection to database closed.")
+        exit()
+    else:
+        print("Whoops! Invalid input. Try again.")
